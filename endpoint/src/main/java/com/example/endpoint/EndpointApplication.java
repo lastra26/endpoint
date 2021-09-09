1d1235c2b4fab68ed538dcce85a116b244402e72
CNBYV\IDEX2261 <IDEX2261@cnbv.gob.mx>
2021-09-09 14:53:39 -0500

    datos para endPoint y dependencias

---
+++ endpoint/src/main/java/com/example/endpoint/EndpointApplication.java
@@ -0,0 +1,15 @@
+package com.example.endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class EndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndpointApplication.class, args);
+		System.out.println("Iniciando App");
+	}
+
+}
---
+++ endpoint/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'andres','guzman','profesor@gmail','2017-06-21');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'jose','perez','lastra261989@gmail','2020-06-21');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (3, 'francisco','lastra','lastra261989@gmail','2021-06-21');
+
+
+
