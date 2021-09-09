3e1ea70ebdb785d5977e017807e51e0a205a265b
SERVIDOR\Servidor <lastra261989@gmail.com>
2021-09-09 18:32:48 -0500

    se actualizo import.sql desde pc servidor

--- endpoint/src/main/resources/import.sql
+++ endpoint/src/main/resources/import.sql
@@ -1,6 +1,10 @@
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'andres','guzman','profesor@gmail','2017-06-21');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'jose','perez','lastra261989@gmail','2020-06-21');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (3, 'francisco','lastra','lastra261989@gmail','2021-06-21');
+INSERT INTO `booking_restaurant`.`booking_restaurant` (`id`, `address`, `description`, `image`, `name`) VALUES ('1', 'loco', 'loco', 'loco', 'loco');
+INSERT INTO `booking_restaurant`.`booking_restaurant` (`id`, `address`, `description`, `image`, `name`) VALUES ('2', 'chido', 'chido', 'chido', 'chido');
+INSERT INTO `booking_restaurant`.`booking_restaurant` (`id`, `address`, `description`, `image`, `name`) VALUES ('3', 'malo', 'malo', 'malo', 'malo');
 
-
-
+INSERT INTO `booking_restaurant`.`turn` (`id`, `name`, `restaurant_id`) VALUES ('1', 'mañana', '1');
+INSERT INTO `booking_restaurant`.`turn` (`id`, `name`, `restaurant_id`) VALUES ('2', 'tarde', '1');
+INSERT INTO `booking_restaurant`.`turn` (`id`, `name`, `restaurant_id`) VALUES ('3', 'mañana', '2');
+INSERT INTO `booking_restaurant`.`turn` (`id`, `name`, `restaurant_id`) VALUES ('4', 'tarde', '2');
+INSERT INTO `booking_restaurant`.`turn` (`id`, `name`, `restaurant_id`) VALUES ('5', 'mañana', '3');
+INSERT INTO `booking_restaurant`.`turn` (`id`, `name`, `restaurant_id`) VALUES ('6', 'tarde', '3');
